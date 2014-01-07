156b5fbc20966af6ff0e4559771da496062108d0
Kurbanismailov Zaur <Kurbanismailov.92@gmail.com>
2014-01-07 20:22:32 +0400

    Настройка url для тестового шаблона и определение моделей

---
+++ MyOrders/middleware.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from django.http import HttpResponseForbidden
+
+__author__ = 'Заур'
+
+
+class MiddleWareProcess(object):
+
+    def process_request(self, request):
+        if not request.user.is_authenticated():
+            return HttpResponseForbidden()
